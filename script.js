10f2548f82bf13812e6097392db30e7b0e1270b5
unknown <biancapeleao@gmail.com>
2023-02-08 21:49:19 -0300

    add components

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const on = document.getElementById('on')
+const divImg = document.getElementById('img-lamp')
+const off = document.getElementById('off')
+
+on.addEventListener('click', ligar)
+off.addEventListener('click', desligar)
+
+
+function ligar() {
+    divImg.classList.add('ligado')
+    divImg.classList.remove('desligado')
+    }
+
+function desligar() {
+    divImg.classList.add('desligado')
+    divImg.classList.remove('ligado')
+    }
